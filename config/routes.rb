334509e10a117463c953e47c8b82f67a316df4d8
Eva Touboulic <etouboulic@gmail.com>
2024-02-27 14:10:07 +0100

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  # => www.grape-and-cheese.com
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
