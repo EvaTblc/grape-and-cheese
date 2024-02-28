53f33af273019aed1961e634a06b3010cfa745fd
Eva Touboulic <etouboulic@gmail.com>
2024-02-28 12:17:15 +0100

    routes et button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # => www.grape-and-cheese.com
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :wines, only: [:index]
 end
