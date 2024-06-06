a04b0058c5a8727b9cbcd8679be03c60730c7274
Eva Touboulic <etouboulic@gmail.com>
2024-06-06 11:28:44 +0200

    fix show

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["togglableElement", "addBtn", "back"]
 
   fire() {
     console.log("ok");
     this.togglableElementTarget.classList.toggle("d-none");
+    this.addBtnTarget.classList.toggle("d-none");
   }
 }
